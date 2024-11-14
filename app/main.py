ba20a292e8220107e2722616967fa31f801009fd
benberinsky <tfu5hw@virginia.edu>
2024-11-14 14:37:16 -0500

    updated to resolve merge conflicts

--- app/main.py
+++ app/main.py
@@ -1,17 +1,54 @@
 #!/usr/bin/env python3
-
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
+import mysql.connector
+from mysql.connector import Error
 import json
 import os
 
-api = FastAPI()
+app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
 
-@api.get("/")  # zone apex
-def zone_apex():
-    return {"Hello": "Hello API"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@api.get("/add/{a}/{b}")
-def add(a: int, b: int):
-    return {"sum": a + b}
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "admin"
+DBPASS = os.getenv('DBPASS')
+DB = "tfu5hw"
+
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+cur=db.cursor()
+
+@app.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
+
+@app.get('/songs')
+def get_songs():
+    query = "SELECT songs.id, songs.title, songs.album, songs.artist, songs.year, songs.file, songs.image, genres.genre AS genre FROM songs JOIN genres ON songs.genre = genres.genreid ORDER BY songs.id;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
